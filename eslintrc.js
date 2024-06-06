74068332c76ec101716bad132c77c82174723be3
hiddenciphers <psolvyns@protonmail.com>
2024-06-06 12:54:47 +1000

    Project Initialised

---
+++ eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'react-app', // Add this to include CRA's default ESLint configuration
+    ],
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 12,
+      sourceType: 'module',
+    },
+    plugins: [
+      'react',
+    ],
+    rules: {
+      // Add your custom rules here
+    },
+  };
+  
